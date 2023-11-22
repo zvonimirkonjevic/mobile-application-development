e66e3aca0b8f926fd526d47ff16066abf1b0515e
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-22 18:08:20 +0100

    write test for class Robot

--- intro_to_kotlin/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_kotlin/MainActivity.kt
+++ intro_to_kotlin/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_kotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package hr.ferit.zvonimirkonjevic.intro_to_kotlin
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import hr.ferit.zvonimirkonjevic.intro_to_kotlin.Data.Robot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
+        var robot = Robot()
+        robot.goUp(10)
+        robot.goDown(1)
+        robot.goRight(6)
+        robot.goLeft(1)
+        robot.getLocation()
     }
 }
