fe4b3a68315dc708b762e92e979a020f2e24d511
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-22 18:16:59 +0100

    change gradle version to 34

--- intro_to_kotlin/app/build.gradle.kts
+++ intro_to_kotlin/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "hr.ferit.zvonimirkonjevic.intro_to_kotlin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hr.ferit.zvonimirkonjevic.intro_to_kotlin"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
