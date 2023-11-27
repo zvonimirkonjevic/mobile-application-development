45e89ef1ef2ecf85e8880191533868c229946914
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-27 11:25:49 +0100

    create Ingredient and Recipe classes

---
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Recipe.kt
@@ -0,0 +1,15 @@
+package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.Data
+
+import androidx.annotation.DrawableRes
+
+data class Recipe (
+    @DrawableRes val image: Int = 0,
+    var title: String = "",
+    var category: String = "",
+    var cookingTime: Int = 0,
+    var energy: Int = 0,
+    var rating: Int = 0,
+    var description: String = "",
+    var reviews: List<String>? = null,
+    var ingredients: List<Ingredient>? = null
+)
--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Robot.kt
+++
@@ -1,4 +0,0 @@
-package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.Data
-
-class Robot {
-}
