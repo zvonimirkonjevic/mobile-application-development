d21a7e6394194bf66e84f0e08aa8d2030235d9e9
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-27 11:34:21 +0100

    create list of recipes inside of Recipe class

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Recipe.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Recipe.kt
@@ -1,15 +1,95 @@
 package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.Data
 
 import androidx.annotation.DrawableRes
+import hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.R
 
 data class Recipe (
     @DrawableRes val image: Int = 0,
     var title: String = "",
     var category: String = "",
-    var cookingTime: Int = 0,
-    var energy: Int = 0,
-    var rating: Int = 0,
+    var cookingTime: String = "",
+    var energy: String = "",
+    var rating: String = "",
     var description: String = "",
-    var reviews: List<String>? = null,
-    var ingredients: List<Ingredient>? = null
+    var reviews: String = "",
+    var ingredients: List<Ingredient>? = null,
+
+    val recipes: List<Recipe> = listOf(
+        Recipe(
+            image = R.drawable.strawberry_pie_1,
+            title = "Strawberry Cake",
+            category = "Desserts",
+            cookingTime = "50 min",
+            energy = "620 kcal",
+            rating = "4,9",
+            description = "This dessert is very tasty and not difficult to prepare. Also, you can replace strawberries with any other berry you like.",
+            reviews = "84 photos 430 comments",
+            ingredients = listOf(
+                Ingredient(R.drawable.flour, "Flour", "450 g"),
+                Ingredient(R.drawable.eggs, "Eggs", "4"),
+                Ingredient(R.drawable.juice, "Lemon juice", "150 g"),
+                Ingredient(R.drawable.strawberry, "Strawberry", "200 g"),
+                Ingredient(R.drawable.suggar, "Sugar", "1 cup"),
+                Ingredient(R.drawable.mint, "Mint", "20 g"),
+                Ingredient(R.drawable.vanilla, "Vanilla", "1/2 teaspoon"),
+            )
+        ),
+        Recipe(
+            image = R.drawable.strawberry_pie_1,
+            title = "Strawberry Cake2",
+            category = "Desserts",
+            cookingTime = "50 min",
+            energy = "620 kcal",
+            rating = "4,9",
+            description = "This dessert is very tasty and not difficult to prepare. Also, you can replace strawberries with any other berry you like.",
+            reviews = "84 photos 430 comments",
+            ingredients = listOf(
+                Ingredient(R.drawable.flour, "Flour", "450 g"),
+                Ingredient(R.drawable.eggs, "Eggs", "4"),
+                Ingredient(R.drawable.juice, "Lemon juice", "150 g"),
+                Ingredient(R.drawable.strawberry, "Strawberry", "200 g"),
+                Ingredient(R.drawable.suggar, "Sugar", "1 cup"),
+                Ingredient(R.drawable.mint, "Mint", "20 g"),
+                Ingredient(R.drawable.vanilla, "Vanilla", "1/2 teaspoon"),
+            )
+        ),
+        Recipe(
+            image = R.drawable.strawberry_pie_1,
+            title = "Strawberry Cake3",
+            category = "Desserts",
+            cookingTime = "50 min",
+            energy = "620 kcal",
+            rating = "4,9",
+            description = "This dessert is very tasty and not difficult to prepare. Also, you can replace strawberries with any other berry you like.",
+            reviews = "84 photos 430 comments",
+            ingredients = listOf(
+                Ingredient(R.drawable.flour, "Flour", "450 g"),
+                Ingredient(R.drawable.eggs, "Eggs", "4"),
+                Ingredient(R.drawable.juice, "Lemon juice", "150 g"),
+                Ingredient(R.drawable.strawberry, "Strawberry", "200 g"),
+                Ingredient(R.drawable.suggar, "Sugar", "1 cup"),
+                Ingredient(R.drawable.mint, "Mint", "20 g"),
+                Ingredient(R.drawable.vanilla, "Vanilla", "1/2 teaspoon"),
+            )
+        ),
+        Recipe(
+            image = R.drawable.strawberry_pie_1,
+            title = "Strawberry Cake4",
+            category = "Desserts",
+            cookingTime = "50 min",
+            energy = "620 kcal",
+            rating = "4,9",
+            description = "This dessert is very tasty and not difficult to prepare. Also, you can replace strawberries with any other berry you like.",
+            reviews = "84 photos 430 comments",
+            ingredients = listOf(
+                Ingredient(R.drawable.flour, "Flour", "450 g"),
+                Ingredient(R.drawable.eggs, "Eggs", "4"),
+                Ingredient(R.drawable.juice, "Lemon juice", "150 g"),
+                Ingredient(R.drawable.strawberry, "Strawberry", "200 g"),
+                Ingredient(R.drawable.suggar, "Sugar", "1 cup"),
+                Ingredient(R.drawable.mint, "Mint", "20 g"),
+                Ingredient(R.drawable.vanilla, "Vanilla", "1/2 teaspoon"),
+            )
+        )
+    )
 )
