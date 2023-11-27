e294ce9590407967469f8eaf5bc4443596294e0e
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-27 11:10:06 +0100

    create project intro-to-jetpack-compose

---
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/MainActivity.kt
@@ -0,0 +1,20 @@
+package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.ui.theme.Intro_to_jetpack_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
