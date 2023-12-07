7eec2b8c57a7abfd39d9fab45fd49dc3f1a63eca
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-07 21:48:58 +0100

    create RecipeDetailsScreen composable

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
@@ -1,2 +1,10 @@
 package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RecipeDetailsScreen() {
+    
+}
