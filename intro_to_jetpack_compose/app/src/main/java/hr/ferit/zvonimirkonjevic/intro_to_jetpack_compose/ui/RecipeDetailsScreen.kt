7d7936acf1904e265d3f9a2244a4800cc2dae7d7
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-07 22:22:58 +0100

    display IngredientCard on RecipeDetailsScreen

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
@@ -1,13 +1,33 @@
 package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.ui
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.R
+import hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.ui.theme.DarkGray
 
 @Preview
 @Composable
 fun RecipeDetailsScreen() {
-    
+    IngredientCard(iconResource = R.drawable.mint, title = "Mint", subtitle = "20g")
 }
 
 @Composable
@@ -16,5 +36,22 @@ fun IngredientCard(
     title: String,
     subtitle: String
 ) {
-    
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(bottom= 16.dp)
+    ){
+        Card (
+            shape = shapes.large,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .width(100.dp)
+                .height(100.dp)
+                .padding(bottom = 8.dp)
+
+        ) {
+            Image(painter = painterResource(id = iconResource), contentDescription = null, Modifier.padding(16.dp))
+            Text(text = title, modifier = Modifier.width(100.dp), fontSize = 14.sp, fontWeight = FontWeight.Medium)
+            Text(text = subtitle, color = DarkGray, modifier = Modifier.width(100.dp), fontSize = 14.sp)
+        }
+    }
 }
