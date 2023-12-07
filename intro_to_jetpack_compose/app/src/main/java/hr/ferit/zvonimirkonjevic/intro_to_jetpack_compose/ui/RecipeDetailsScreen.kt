e80da259bed31e2555f4d46d0f95f6aa732982fb
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-07 21:50:25 +0100

    create IngredientCard composable

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/ui/RecipeDetailsScreen.kt
@@ -1,10 +1,20 @@
 package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.ui
 
+import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun RecipeDetailsScreen() {
+    
+}
+
+@Composable
+fun IngredientCard(
+    @DrawableRes iconResource: Int,
+    title: String,
+    subtitle: String
+) {
     
 }
