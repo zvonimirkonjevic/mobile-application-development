61a996a68f8132ab3a1b1600ac1f55f576a41caf
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-12 14:53:30 +0100

    create ScreenInfo composable function

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/RecipeViewModel.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/RecipeViewModel.kt
@@ -1,2 +1,30 @@
 package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.Data
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+
+class RecipeViewModel: ViewModel() {
+    private val db = Firebase.firestore
+    val recipesData = mutableStateListOf<Recipe>()
+
+    init{
+        fetchDatabaseData()
+    }
+
+    private fun fetchDatabaseData(){
+        db.collection("recipes")
+            .get()
+            .addOnSuccessListener { result ->
+                for (data in result.documents){
+                    val recipe = data.toObject(Recipe::class.java)
+                    if (recipe != null){
+                        recipe.id = data.id
+                        recipesData.add(recipe)
+                    }
+                }
+            }
+    }
+}
