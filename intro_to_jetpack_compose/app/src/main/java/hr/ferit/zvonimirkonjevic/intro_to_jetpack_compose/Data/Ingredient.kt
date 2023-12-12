9a4edce1570c87bdc73b75f681620fcc0753b81e
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-12 15:02:56 +0100

    rework classes Recipe and Ingredient

--- intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Ingredient.kt
+++ intro_to_jetpack_compose/app/src/main/java/hr/ferit/zvonimirkonjevic/intro_to_jetpack_compose/Data/Ingredient.kt
@@ -2,8 +2,8 @@ package hr.ferit.zvonimirkonjevic.intro_to_jetpack_compose.Data
 
 import androidx.annotation.DrawableRes
 
-data class Ingredient (
-    @DrawableRes val image: Int,
-    var name: String = "",
-    var subtitle: String = ""
+data class Ingredient(
+    val image: String = "",
+    val title: String = "",
+    val subtitle: String = ""
 )
